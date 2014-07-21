2da8a1db06d713496bca4ebe349e67a955fd5258
Chris Montrois <cmontrois@quickleft.com>
2014-07-21 09:50:43 -0600

    Accepting process port

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.get('/list/:listid/item/:itemid', function(req, res) {
   res.sendfile('public/index.html');
 });
 
-var server = app.listen(8080, function() {
+var port = Number(process.env.PORT || 8080);
+var server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
